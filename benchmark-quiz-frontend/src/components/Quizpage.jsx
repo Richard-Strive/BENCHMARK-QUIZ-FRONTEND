94cebf2c932072bac6a47ae684eaed1a9ae40612
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-01-10 21:13:58 +0100

    Ok

--- benchmark-quiz-frontend/src/components/Quizpage.jsx
+++ benchmark-quiz-frontend/src/components/Quizpage.jsx
@@ -5,54 +5,100 @@ import "./Quizpage.css";
 export default function Quiz({ id }) {
   const [quiz, setQuiz] = useState([]);
 
-  const url = `http://localhost:5000/exam/${id}/answer`;
+  const quest = {
+    providedAnswer: 1,
+    selectedQuestion: 3,
+  };
 
   const getQuiz = async () => {
     try {
-      const response = await fetch(url, {
-        method: "POST",
+      const response = await fetch(`http://localhost:5000/exam/${id}`, {
+        method: "GET",
         mode: "cors",
+
         header: {
           "Content-type": "application/json",
           accept: "application/json",
         },
-        body: JSON.stringify(),
+        // body: JSON.stringify(quest),
       });
-      console.log(await response);
+
+      const data = await response.json();
+
+      console.log(data);
+      if (response.ok) {
+        setQuiz(data);
+      }
+
+      console.log(await response.json());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const postAnswer = async (index) => {
+    try {
+      const answer = {
+        providedAnswer: index,
+      };
+
+      const response = await fetch(`http://localhost:5000/exam/${id}/answer`, {
+        method: "POST",
+        mode: "cors",
+
+        header: {
+          "Content-type": "application/json",
+          accept: "application/json",
+        },
+        body: JSON.stringify(quest),
+      });
+
+      const data = await response.json();
+
+      console.log(data);
+      if (response.ok) {
+        setQuiz(data);
+      }
+
+      console.log(await response.json());
     } catch (error) {
       console.log(error);
     }
   };
 
-  useEffect(() => {
-    getQuiz();
-  });
+  // useEffect(() => {
+  //   getQuiz();
+  // }, [quiz]);
 
   return (
-    <div className="quiz_page">
-      <div className="quiz_question">
-        <h1 className="animate__animated animate__backInDown">
-          I AM THE QUESTION
-        </h1>
-      </div>
-      <div className="quiz_row">
-        <div className="question_one">
-          {" "}
-          <p>Bla Bla Bla</p>
+    <>
+      {!quiz ? (
+        "LOADING"
+      ) : (
+        <div className="quiz_page">
+          <div className="quiz_question">
+            <h1 className="animate__animated animate__backInDown title">
+              {/* {`${quiz.questions[0].text}`} */}
+            </h1>
+          </div>
+          <div className="quiz_row">
+            <div className="question_one" onClick={getQuiz()}>
+              {/* <p>{`${quiz.questions[0].answers[0].text}`}</p> */}
+            </div>
+            <div className="question_two" onClick={() => postAnswer(2)}>
+              {/* <p>{`${quiz.questions[0].answers[1].text}`}</p> */}
+            </div>
+          </div>
+          <div className="quiz_row">
+            <div className="question_one" onClick={() => postAnswer(3)}>
+              {/* <p>{`${quiz.questions[0].answers[2].text}`}</p> */}
+            </div>
+            <div className="question_two" onClick={() => postAnswer(4)}>
+              {" "}
+              {/* <p>{`${quiz.questions[0].answers[3].text}`}</p> */}
+            </div>
+          </div>
         </div>
-        <div className="question_two">
-          <p>Bla Bla Bla </p>
-        </div>
-      </div>
-      <div className="quiz_row">
-        <div className="question_one">
-          <p>Bla Bla Bla </p>
-        </div>
-        <div className="question_two">
-          {" "}
-          <p>Bla Bla Bla</p>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
